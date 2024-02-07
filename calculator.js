f25aa4a221e62173f0db625988918bd7271f158c
Trevor <troferns040@gmail.com>
2024-02-07 23:59:55 +0000

    amended calculator backend. Added delete button and ability to operate on decimal numbers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-const nothing = function() {return }
-
 const add = function(a, b) {
     return a+b;
 }
@@ -18,7 +16,7 @@ const divide = function(a, b) {
 }
 
 const operate = function(operand, a, b) {
-    let answer = operand(parseInt(a), parseInt(b));
+    let answer = operand(Number(a), Number(b));
     screen.innerHTML = answer;
     num2 = ""
     num1 = answer
@@ -30,36 +28,31 @@ let operations = {"add": add, "minus": subtract, "multiply": multiply, "divide":
 
 let num1 = ""
 let num2 = ""
-let operator = nothing
+let operator = null
 const screen = document.getElementById("screen");
 
 const clear = document.getElementById("clear")
 clear.addEventListener("click", () => {
     num1 = ""
     num2 = ""
-    operator = nothing
+    operator = null
     screen.innerHTML = ""
 })
 
 const equals = document.getElementById("equals")
-equals.addEventListener("click", () => {
-    operate(operator, num1, num2)
-}); 
+equals.addEventListener("click", () => operate(operator, num1, num2)); 
 
 const nums = document.querySelectorAll(".nums")
 nums.forEach(num => {
     num.addEventListener("click", () => {
-        console.log(`bef nums ${num1}, ${num2}`)
-        if (operator != nothing) {
-            
+        if (operator != null) {
             num2 += num.value
             screen.innerHTML = num2;
         }
         else {
             num1 += num.value
             screen.innerHTML = num1;
-        } 
-        console.log(`aft nums ${num1}, ${num2}`)
+        }
     })
     
 });
@@ -67,11 +60,33 @@ nums.forEach(num => {
 const operation_buttons = document.querySelectorAll(".operations")
 operation_buttons.forEach(btn => {
     btn.addEventListener("click", () => {
-        if (operator != nothing && num2 != "") {
+        if (operator != null && num2 != "") {
             operate(operator, num1, num2)
-
         }
-        operator = operations[btn.id]
+        if (num1 != "") operator = operations[btn.id];
     })
 })
 
+const delete_btn = document.querySelector("#delete")
+delete_btn.addEventListener("click", () => {
+    if (operator == null && num1.length > 0) {
+        num1 = num1.slice(0, -1)
+        screen.innerHTML = num1
+    }
+    else if (num2.length > 0) {
+        num2 = num2.slice(0, -1);
+        screen.innerHTML = num2
+    }
+})
+
+const decimal = document.querySelector("#decimal")
+decimal.addEventListener("click", () => {
+    if (operator != null && !num2.includes(".")) {
+        num2 += "."
+        screen.innerHTML = num2;
+    }
+    else if (!num1.includes(".")){
+        num1 += "."
+        screen.innerHTML = num1;
+    }
+})
